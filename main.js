7dead22d8c98fa6ea13976ca81dcb437558e73c8
Thiago Guerrero <tgbalera@hotmail.com>
2021-10-23 14:11:53 -0300

    finish #1
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>

---
+++ main.js
@@ -0,0 +1,32 @@
+import Problem1 from './problem1.js'
+import readline from 'readline'
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Escolha um exercício para rodar: ', exercise => {
+  switch (exercise) {
+    case '1':
+      run(Problem1);
+      break;
+    default:
+      console.log('ERROR: Exercício não encontrado!');
+      break;
+  }
+  rl.close();
+});
+
+function run(problemClass) {
+  const correct = problemClass.tests.reduce((acc, test, i) => {
+    const passed = test();
+    if (!passed) console.log(`Teste ${i+1} falhou!`);
+    return acc && passed;
+  }, true);
+
+  if (correct)
+    console.log(`Todos os ${problemClass.tests.length} testes passaram!`);
+  else
+    console.log('Os testes falharam!')
+}
